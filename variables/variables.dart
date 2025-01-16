d22bcfd4250e91a18a55b5bf7140bc802c68087d
Wabwi <wabwiibrahim@gmail.com>
2025-01-16 07:48:48 +0300

    dart variables

---
+++ variables/variables.dart
@@ -0,0 +1,22 @@
+void main() {
+  var nick_name = 'Wabwi';
+  Object first_name = 'Ibra';
+  String last_name = 'hamis';
+  String? tribe = 'Kikuyu'; //Nullable type. Can be 'null' or string.
+  int age = 30;
+  double height;
+  height = 5.6;
+
+  //Late Variables - Variables that are declared without being initialized.
+  late int weight;
+
+  //Final and Const Variables
+  final mothers_name = 'Mauba';
+  const String fathers_name = 'Hamis';
+
+  weight = 70;
+
+  print('This is, $nick_name $first_name $last_name, He is a $tribe!');
+  print('He is $age years old and $height feet tall and weighs $weight kgs');
+  print('His mother is $mothers_name and his father is $fathers_name');
+}
