a9d46b4643ffe641571ad2920a9f2695b94eb508
Wabwi <wabwiibrahim@gmail.com>
2025-01-20 05:26:22 +0300

    operators

--- operators/operators.dart
+++ operators/operators.dart
@@ -1,41 +1,98 @@
-import 'dart:io';
-
 void main() {
-  var result;
-  String? output;
-  late double first_number;
-  late double second_number;
+  //Arithmentic operators
+  // declaring two numbers
+    int num1 = 10;
+    int num2 = 3;
 
-  //Arithmetic Operators
-  print('Calculator');
+    // performing arithmetic calculation
+    int sum = num1 + num2; // addition
+    int diff = num1 - num2; // subtraction
+    int unaryMinus = -num1; // unary minus
+    int mul = num1 * num2; // multiplication
+    double div = num1 / num2; // division
+    int div2 = num1 ~/ num2; // integer division
+    int mod = num1 % num2; // show remainder
 
-  //get user input
-  print('Enter first number');
-  first_number = double.parse(stdin.readLineSync()!);
-  
+  //Printing info
+    print("The addition is $sum.");
+    print("The subtraction is $diff.");
+    print("The unary minus is $unaryMinus.");
+    print("The multiplication is $mul.");
+    print("The division is $div.");
+    print("The integer division is $div2.");
+    print("The modulus is $mod.");
 
-  //get operator symbon as input from user
-  print('Enter + or - or * or /');
-  var operator = stdin.readLineSync()!;
 
-  print('Enter second number');
-  second_number = double.parse(stdin.readLineSync()!);
 
-  if (operator == '+') {
-    result = (first_number + second_number);
-    output = 'Sum of';
-  } else if (operator == '-') {
-    result = (first_number - second_number);
-    output = 'Difference between';
-  } else if (operator == '*') {
-    result = (first_number * second_number);
-    output = 'Product of';
-  } else if (operator == '/') {
-    result = (first_number / second_number);
-    output = 'Quotient of';
-  } else {
-    print('Invalid operator');
-  }
+  //Increament and Decreament operators
+  // declaring two numbers 
+  int incrementNum1 = 0;
+  int incrementNum2 = 0;
 
-  print('$output $first_number and $second_number is $result');
+  // performing increment / decrement operator
+
+  // pre increment
+  incrementNum2 = ++incrementNum1;
+  print("The value of incrementNum2 is $incrementNum2");
+
+  // reset value to 0
+  incrementNum1 = 0;
+  incrementNum2 = 0;
+
+  // post increment
+  incrementNum2 = incrementNum1++;
+  print("The value of incrementNum2 is $incrementNum2");
+
+
+
+
+  //Relational operators
+  int relNum1 = 10;
+  int relNum2 = 5;
+  //printing info
+  print(relNum1 == relNum2);
+  print(relNum1 < relNum2);
+  print(relNum1 > relNum2);
+  print(relNum1 <= relNum2);
+  print(relNum1 >= relNum2);
+
+
+
+
+  //Logical operators
+  int userid = 123;
+  int userpin = 456;
+
+  // Printing Info
+  print((userid == 123) && (userpin == 456)); // print true
+  print((userid == 1213) && (userpin == 456)); // print false.
+  print((userid == 123) || (userpin == 456)); // print true.
+  print((userid == 1213) || (userpin == 456)); // print true
+  print((userid == 123) != (userpin == 456)); //print false
+
+
+
+
+
+  //Assignment operators
+  double age = 24;
+  age += 1; // Here age+=1 means age = age + 1.
+  print("After Addition Age is $age");
+  age -= 1; //Here age-=1 means age = age - 1.
+  print("After Subtraction Age is $age");
+  age *= 2; //Here age*=2 means age = age * 2.
+  print("After Multiplication Age is $age");
+  age /= 2; //Here age/=2 means age = age / 2.
+  print("After Division Age is $age");
+
+
+
+
+
+  //Type test operators
+  String value1 = "Dart Tutorial";
+  int randomNumber = 10;
+
+  print(value1 is String);
+  print(randomNumber is! int);
 }
